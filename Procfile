c49a6d2320405735f2800e237caf60d98d3ad33f
DeepakChakravarthy <dcdeepak2000@rediffmail.com>
2021-03-24 22:26:06 +0530

    Changes for deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ipc_panel.wsgi
--- ipc_panel/settings.py
+++ ipc_panel/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -48,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ipc_panel.urls'
@@ -117,5 +121,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+ 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'Whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
