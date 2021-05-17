412f6795bde4354ba956f5475633bb07481cca5d
Ramkumar <36497508+RamkumarMrj@users.noreply.github.com>
2021-05-17 23:36:46 +0530

    Update settings.py

--- ipc_panel/settings.py
+++ ipc_panel/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'bkr(1mx&+l80n275l=j6rz!4@qzfb$^-n86xqc*+sj9b*kz9gm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
