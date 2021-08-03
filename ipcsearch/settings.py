f53985a9ed5901c4c32c63326dcc71c5a50ccd1d
Deepak Chakravarthy <42644785+DeepakChakravarthy@users.noreply.github.com>
2021-08-03 10:30:40 +0530

    Update settings.py

--- ipcsearch/settings.py
+++ ipcsearch/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'search/templates/search/static'
 STATICFILES_DIRS = (str(BASE_DIR.joinpath('search/templates/search/static')),)
 
 # Default primary key field type
