3a56b5edcf0d3ce9b4f8a145e35624ea8a4d1bc5
RamkumarMrj <ramkumar.mrj97@gmail.com>
2021-08-08 23:36:43 +0530

    deploy

--- ipcsearch/settings.py
+++ ipcsearch/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +25,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1@jcu0nc!-a8-@upkd$j1&-(@+nmjz9b%m+7o81auwxd59uxrc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'https://ipcfinder.herokuapp.com/',
+    'localhost',
+    '127.0.0.1'
+]
 
 
 # Application definition
@@ -125,3 +131,8 @@ STATICFILES_DIRS = (str(BASE_DIR.joinpath('search/templates/search/static')),)
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = (str(BASE_DIR.joinpath('search/templates/search/static')),)
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
