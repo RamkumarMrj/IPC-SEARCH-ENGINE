ec7068d0cea502e4f73360598e5ed4f4bf63a683
RamkumarMrj <ramkumar.mrj97@gmail.com>
2022-07-27 23:56:35 +0530

    cache removed

--- ipcsearch/settings.py
+++ ipcsearch/settings.py
@@ -118,8 +118,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Static files (CSS, JavaScript, Images)
